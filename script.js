fe9cb0c7b8062bb67fe5a6d06e3ad34d5979ea0e
NathanM <nathan.milburn@outlook.com>
2022-02-11 14:21:32 -0800

    added some extra styling, created variables for my js sheet

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var myAPIKey = "830e2296d28e9adef700a0677aa768ed";
 var citySelect = document.querySelector('#area-search');
+var selectionHistory = document.querySelector("#city-results");
+var cityResults = document.querySelector('#city-results');
+var currentForecast = document.querySelector("#city-name");
+var currentTemp = document.querySelector("#currentTemp");
+var currentWind = document.querySelector("#currentWind");
+var currentHumidity = document.querySelector("#currentHumidity");
+var currentUVIndex = document.querySelector("#currentUVIndex");
 var today = moment().format('L');
+var dailyIcon = document.querySelector(".dailyIcon");
+var timeOffset;
 // var city = citySelect.val();
 var city = 'Seattle'
 var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myAPIKey}`;
-
 
 function pullWeatherData() {
 fetch(queryURL)
